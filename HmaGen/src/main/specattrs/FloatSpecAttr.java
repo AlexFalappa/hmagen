5d9a1ecadafa1cc9a56aee987593e7810421d71c
Alessandro Falappa <alex.falappa@gmail.com>
2014-09-28 12:19:38 +0200

    Corrected copyright name in header

--- HmaGen/src/main/specattrs/FloatSpecAttr.java
+++ HmaGen/src/main/specattrs/FloatSpecAttr.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 sasha.
+ * Copyright 2014 Alessandro Falappa.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
